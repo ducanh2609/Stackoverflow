f47d1aa1c433a8f13503a43dfe5c5a091994a1a5
daijja <buiminh8690@gmail.com>
2023-02-13 11:20:42 +0700

    alooooooo

---
+++ Frontend/src/liteComponents/HompageContent.jsx
@@ -0,0 +1,7 @@
+export default function HomePageContent() {
+    return ( 
+        <div>
+            <div>adhasdhdada</div>
+        </div>
+    )
+}
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import HomePageContent from "../liteComponents/HompageContent";
+import Footer from './../components/Footer';
 
 export default function HomePage() {
   return (
     <>
       <Header />
+      <HomePageContent/>
+      <Footer/>
     </>
   );
 }
